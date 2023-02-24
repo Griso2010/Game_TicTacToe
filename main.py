455fc03fdaa9fe00ff3873a986be504f41985173
Grigoriy <sokolov.g.2016@gmail.com>
2023-02-24 21:45:18 +0200

    Version 2.0.Fixed bugges, cleaed code

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+from random import shuffle
 from tkinter import *
 from  tkinter import messagebox
+
 
 FIRST_USER_SYMBOL = "x"
 SECOND_USER_SYMBOL = "0"
 current_player = FIRST_USER_SYMBOL
 bg = "#FA8072" # Color of backgroud 
 
-
-
 # Root settings
 root = Tk()
+
 root.title("TicTactoe")
 root.geometry("800x700+10+100")
 root.config(bg=bg)
@@ -51,16 +52,33 @@ def create_board():
             game_board[row][col].grid(column=col, row=row, ipadx=30, ipady=30, padx=5, pady=5, sticky='snew')
 
 def remove_board():
+    global current_player 
     # Board remove
     size = 3
     for row in range(size):
         for col in range(size):
             game_board[row][col].grid_forget()  
-
+    error.grid_forget()
+    turn.grid_forget()
+    current_player = FIRST_USER_SYMBOL
     create_board()
+    
+def check_player_1():
+    global name_of_player_1
+    if len(entry_1.get()) == 0: # If name of player 1 is empty
+        name_of_player_1 = "x" # Name player 1 is zero
+    else:    
+        name_of_player_1 = entry_1.get().title() # Get player 1 name
+
+def check_player_2():
+    global name_of_player_2
+    if len(entry_2.get()) == 0: 
+        name_of_player_2 = "0"
+    else:    
+        name_of_player_2 = entry_2.get().title() 
 
 def input_control(event):
-    global game_board, current_player, size
+    global game_board, current_player, size, counter_1, counter_2
     current_widget = event.widget  # Field
     error.config(text="", fg="black")
     field_value = current_widget.get()  # Get a value from a field now
@@ -78,30 +96,42 @@ def input_control(event):
             for e in row:
                 # Blocking free fields
                 e.config(state="disabled")  
-        messagebox.showinfo("Tic Tac Toe", f"Congratulation to player: {current_player}\nYou Win!") # Show a win
+
+
+       
+
+        for c in range(len(game_board)):
+            for r in range(len(game_board)):
+                if game_board[r][c] != " ":
+                    print("Empty")
+
+        if current_player == 'x':
+            check_player_1()
+            messagebox.showinfo("Tic Tac Toe", f"Congratulation to player: {name_of_player_1}\nYou Win!") # Show a win
+     
+            
+            
+            
+        if current_player == "0":
+            check_player_2()
+            messagebox.showinfo("Tic Tac Toe", f"Congratulation to player: {name_of_player_2}\nYou Win!") # Show a win
+ 
+            
         remove_board()
         return
     
     # Check turns
     if current_player == FIRST_USER_SYMBOL:
-        if len(entry_2.get()) == 0: # If name of player 2 is empty
-            name_of_player_2 = "0" # Name player 2 is zero
-        else:    
-            name_of_player_2 = entry_2.get().title() # Get player 2 name
-   
-        turn.config(text=f"Turn of {name_of_player_2}", fg="black") # Next turn
         current_player = SECOND_USER_SYMBOL
-
+        check_player_2()
+        turn.config(text=f"Turn of {name_of_player_2}", fg="black") 
     else:
         current_player = FIRST_USER_SYMBOL
-        if len(entry_1.get()) == 0: # If name of player 1 is empty
-            name_of_player_1 = "x" # Name player 2 is X
-        else:
-            name_of_player_1 = entry_1.get().title() # Get player 2 name
-
-        turn.config(text=f"Turn of {name_of_player_1}", fg="black") # Next turn
-
+        check_player_1()
+        turn.config(text=f"Turn of {name_of_player_1}", fg="black") 
+        
 def check_winner():
+    
     for row in game_board:
         if row[0].get() == '': continue
         if len(set(map(lambda e: e.get(), row))) == 1: return True
@@ -132,17 +162,17 @@ def create_game():
 
     button_enter.grid(row=4, column=1, pady=5)
 
+def standoff():
 
+    pass
 
 # Entry and Labels
 name_label = Label(top_frame, text="TicTacToe", font=('Times New Roman', 25, 'bold'), bg=bg, fg='white')
 name_owner = Label(top_frame, text="©Grigoriy Sokolov", font=('Times New Roman', 20, "bold"), bg=bg, fg='white')
-name_player_1 = Label(top_frame, text="Player 1", font=('Times New Roman', 20, "bold"), bg=bg, fg='black')
-name_player_2 = Label(top_frame, text="Player 2", font=('Times New Roman', 20, "bold"), bg=bg, fg='black')
+name_player_1 = Label(top_frame, text="Player 1", font=('Times New Roman', 20, "bold"), bg=bg, fg='white')
+name_player_2 = Label(top_frame, text="Player 2", font=('Times New Roman', 20, "bold"), bg=bg, fg='white')
 entry_1 = Entry(top_frame)
 entry_2 = Entry(top_frame)
-
-current_player_1 = entry_1.get()
 
 # Buttons
 new_game = Button(top_frame, text="New Game", font=('Times New Roman', 20), command=create_game)
@@ -166,5 +196,6 @@ new_game.grid(row=0, column=1, padx=10)
 clear_board.grid(row=0, column=4, padx=10)
 
 root.mainloop()
+
 
 
